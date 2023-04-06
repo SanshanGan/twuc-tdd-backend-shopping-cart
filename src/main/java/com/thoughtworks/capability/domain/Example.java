f06e624404ea8060f64db983f942fc7fedc17722
Sanshan <105403665+SanshanGan@users.noreply.github.com>
2023-04-06 22:27:23 +0800

    Update Example.java
    [sanshan]chore: duplicate line6 and add new line in line27

--- src/main/java/com/thoughtworks/capability/domain/Example.java
+++ src/main/java/com/thoughtworks/capability/domain/Example.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+//import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,5 +24,6 @@ public class Example {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    private Long items;
 
 }
